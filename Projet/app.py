42a227fba8cd77f314cb565c18f5b8e1bb4fc558
kklisto <kwillaey@gmail.com>
2023-04-06 15:56:44 +0200

    Test

---
+++ Projet/app.py
@@ -0,0 +1,15 @@
+from cassandra.cluster import Cluster
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+cluster = Cluster(['localhost'])
+session = cluster.connect('rss_data')
+
+@app.route('/')
+def home():
+    rows = session.execute('SELECT * FROM lemonde_data LIMIT 10')
+    return render_template('index.html', rows=rows)
+
+if __name__ == '__main__':
+    app.run(debug=True)
